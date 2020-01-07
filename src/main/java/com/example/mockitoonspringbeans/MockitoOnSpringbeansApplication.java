f47e5101a278ce8f59670890176d9736e414b057
Maureen Josephine <Maureen Josephine>
2020-01-07 17:56:39 +0300

    initial commit

---
+++ src/main/java/com/example/mockitoonspringbeans/MockitoOnSpringbeansApplication.java
@@ -0,0 +1,13 @@
+package com.example.mockitoonspringbeans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockitoOnSpringbeansApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockitoOnSpringbeansApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mockitoonspringbeans/MockitoOnSpringbeansApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mockitoonspringbeans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockitoOnSpringbeansApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

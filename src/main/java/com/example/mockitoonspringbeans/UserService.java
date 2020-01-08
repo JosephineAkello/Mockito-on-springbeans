1756ef804dd3175840673aa9069eaeed5f405e61
Maureen Josephine <Maureen Josephine>
2020-01-08 10:02:32 +0300

    inject nameservice in userservice

--- src/main/java/com/example/mockitoonspringbeans/UserService.java
+++ src/main/java/com/example/mockitoonspringbeans/UserService.java
@@ -1,4 +1,17 @@
 package com.example.mockitoonspringbeans;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class UserService {
+    private NameService nameService;
+
+    @Autowired
+    public UserService(NameService nameService){
+        this.nameService = nameService;
+    }
+
+    public String getUserName(String id){
+        return nameService.getUserName(id);
+    }
+
 }
